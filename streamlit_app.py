9f52c86a2ce0a82bb4e3286e22ad9507a8a9c427
幾個秋 <tewei0328@gmail.com>
2025-03-17 22:45:56 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,20 +9,20 @@ import matplotlib.pyplot as plt
 ticker = '2330.TW'
 
 # 顯示標題
-st.title(f"{ticker} 台積電股價走勢")
+st.title(f"{ticker} 2330.TW")
 
 # 下載資料
 data = yf.download(ticker, period='3y')
 
 # 顯示股價資料
-st.write("股價資料：", data.tail())
+st.write("Stock Price：", data.tail())
 
 # 繪製收盤價走勢圖
 fig, ax = plt.subplots(figsize=(12, 6))
-ax.plot(data.index, data['Close'], label='收盤價', color='blue')
-ax.set_title(f'{ticker} 台積電股價走勢 - 過去3年')
-ax.set_xlabel('日期')
-ax.set_ylabel('收盤價 (TWD)')
+ax.plot(data.index, data['Close'], label='Close', color='blue')
+ax.set_title(f'{ticker} 2330.TW - lasted 3 years')
+ax.set_xlabel('date')
+ax.set_ylabel('close (TWD)')
 ax.legend()
 ax.grid(True)
 st.pyplot(fig)
